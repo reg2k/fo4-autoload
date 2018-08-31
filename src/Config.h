c77869b9b66a37c6bee4efb57bc75a17e4588bdf
reg2k <23437153+reg2k@users.noreply.github.com>
2018-08-31 23:51:14 +0100

    Build for F4SE v0.6.12 (game version v1.10.111)

--- src/Config.h
+++ src/Config.h
@@ -4,8 +4,8 @@
 //-----------------------
 // Plugin Information
 //-----------------------
-#define PLUGIN_VERSION              10
-#define PLUGIN_VERSION_STRING       "1.5.4"
+#define PLUGIN_VERSION              11
+#define PLUGIN_VERSION_STRING       "1.5.5"
 #define PLUGIN_NAME_SHORT           "autoload"
 #define PLUGIN_NAME_LONG            "Autoload"
 #define SUPPORTED_RUNTIME_VERSION   CURRENT_RELEASE_RUNTIME
